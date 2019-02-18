5b56399322501b1b6309ddfdb2d74f9fbde79bd1
Tomas Garcia <tomasotano25@gmail.com>
2019-02-18 11:00:48 -0400

    add products controller

---
+++ EcommerceRestaurant.Web/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿namespace EcommerceRestaurant.Web.Data
+{
+    using EcommerceRestaurant.Web.Data.Entities;
+    using Microsoft.EntityFrameworkCore;
+    // Ctrl + .
+
+    public class DataContext: DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options): base(options)
+        {
+
+        }
+    }
+}
