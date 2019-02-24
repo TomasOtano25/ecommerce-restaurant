233c9d1b78216197692dd4d44fd6e3f643a0e0a0
Tomas Garcia <tomasotano25@gmail.com>
2019-02-24 14:02:26 -0400

    add register user

--- EcommerceRestaurant.Web/Models/RegisterViewModel.cs
+++ EcommerceRestaurant.Web/Models/RegisterViewModel.cs
@@ -24,6 +24,7 @@ namespace EcommerceRestaurant.Web.Models
         public string Password { get; set; }
 
         [Required]
+        [Compare("Password")]
         public string Confirm { get; set; }
     }
 }
