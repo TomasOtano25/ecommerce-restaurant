0d7f563992bcfef6193c6850fe2b3f552539867e
Tomas Garcia <tomasotano25@gmail.com>
2019-02-24 13:15:57 -0400

    add api service

--- EcommerceRestaurant.Common/Services/ApiService.cs
+++ EcommerceRestaurant.Common/Services/ApiService.cs
@@ -6,9 +6,45 @@
     using System.Threading.Tasks;
     using Models;
     using Newtonsoft.Json;
-    
+    using Plugin.Connectivity;
+
     public class ApiService
     {
+        public async Task<Response> CheckConnection()
+        {
+
+            CrossConnectivity.Current.ConnectivityChanged += (sender, args) => 
+            {
+
+            };
+
+            if (!CrossConnectivity.Current.IsConnected)
+            {
+                return new Response
+                {
+                    IsSuccess = false,
+                    Message = "Please turn on your internet settings."
+                };
+                
+            }
+
+            var isReachable = await CrossConnectivity.Current.IsRemoteReachable("google.com");
+            if (!isReachable)
+            {
+                return new Response
+                {
+                    IsSuccess = false,
+                    Message = "No internet connection."
+                };
+            }
+
+            return new Response
+            {
+                IsSuccess = true
+            };
+        }
+
+
         public async Task<Response> GetListAsync<T>(string urlBase, 
             string servicePrefix, 
             string controller)
